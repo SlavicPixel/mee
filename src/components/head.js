fc13652ab139b3da149df56cda4ec6eb8bf37c71
pixel <domi9132@gmail.com>
2023-12-16 23:28:39 +0100

    Updated head to correspond to updated gatsby config

--- src/components/head.js
+++ src/components/head.js
@@ -16,9 +16,6 @@ const Head = ({ title, description, image }) => {
           siteMetadata {
             defaultTitle: title
             defaultDescription: description
-            siteUrl
-            defaultImage: image
-            twitterUsername
           }
         }
       }
